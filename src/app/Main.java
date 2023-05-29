663d3d48a43e6f5da7ff47498ae491b128d4cdb5
DimaProhorenko <dima.study.web@gmail.com>
2023-05-29 20:05:26 +0300

    ADD multiplication

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("My git app.Super!");
@@ -8,10 +10,14 @@ public class Main {
         double firstNum = scanner.nextDouble();
         System.out.println("Enter second number: ");
         double secondNum = scanner.nextDouble();
-        System.out.println(add(firstNum, secondNum));
+
     }
 
     public static double add(double a, double b) {
         return a + b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
     }
 }
