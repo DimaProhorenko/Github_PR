2efec6b8a62d6f392e7a942c06615ddb47f3e69f
DimaProhorenko <dima.study.web@gmail.com>
2023-05-29 20:00:36 +0300

    ADD addition functionality

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,17 @@
 package app;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My git app.Super!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double firstNum = scanner.nextDouble();
+        System.out.println("Enter second number: ");
+        double secondNum = scanner.nextDouble();
+        System.out.println(add(firstNum, secondNum));
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
     }
 }
